d3d5f0bd5062e9b33ac51bec8174f79c96293aa2
Nathaniel H. Arciaga <ty202500699@wmsu.edu.ph>
2026-03-16 10:37:50 +0800

    Create Assignment_002.cpp

---
+++ Assignment_002.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int students, subjects;
+
+    cout << "Enter number of students: ";
+    cin >> students;
+    cout << "Enter number of subjects: ";
+    cin >> subjects;
+
+    int grades[students][subjects];
+    int highest = 0;
+
+    for (int i = 0; i < students; i++) {
+        for (int j = 0; j < subjects; j++) {
+            cout << "Enter " << subjects << " grades for Student " << i + 1 << ": ";
+            cin >> grades[i][j];
+        }
+    }
+    cout << "\n---Grades Matrix---\n";
+    cout << endl;
+    int* ptr = (int*)grades;
+
+    for (int i = 0; i < students; i++) {
+        float sum = 0;
+        cout << "Student " << i + 1 << ": ";
+
+        for (int j = 0; j < subjects; j++) {
+            int currentGrade = *(ptr + (i * subjects) + j);
+            cout << currentGrade << "\t";
+
+            sum += currentGrade;
+            if (currentGrade > highest) highest = currentGrade;
+        }
+
+        cout << "| Average: " << (sum / subjects) << endl;
+    }
+    cout << "\nHighest Grade in Matrix: " << highest << endl;
+
+    return 0;
+}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int students, subjects;
+
+    cout << "Enter number of students: ";
+    cin >> students;
+    cout << "Enter number of subjects: ";
+    cin >> subjects;
+
+    int grades[students][subjects];
+    int highest = 0;
+
+    for (int i = 0; i < students; i++) {
+        for (int j = 0; j < subjects; j++) {
+            cout << "Enter " << subjects << " grades for Student " << i + 1 << ": ";
+            cin >> grades[i][j];
+        }
+    }
+    cout << "\n---Grades Matrix---\n";
+    cout << endl;
+    int* ptr = (int*)grades;
+
+    for (int i = 0; i < students; i++) {
+        float sum = 0;
+        cout << "Student " << i + 1 << ": ";
+
+        for (int j = 0; j < subjects; j++) {
+            int currentGrade = *(ptr + (i * subjects) + j);
+            cout << currentGrade << "\t";
+
+            sum += currentGrade;
+            if (currentGrade > highest) highest = currentGrade;
+        }
+
+        cout << "| Average: " << (sum / subjects) << endl;
+    }
+    cout << "\nHighest Grade in Matrix: " << highest << endl;
+
+    return 0;
+}
